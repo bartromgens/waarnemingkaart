27a8a0d4e8f48d4d9a40ac0e50d288f702d4ec5d
Bart Römgens <bart.romgens@gmail.com>
2017-09-01 14:26:07 +0200

    create observation admin

--- observation/admin.py
+++ observation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from observation.models import Observation
+from observation.models import Group
+from observation.models import Family
+from observation.models import Species
+
+
+class ObservationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'species',
+        'family',
+        'group',
+        'number',
+        'datetime',
+        'coordinates',
+        'url',
+    )
+
+
+admin.site.register(Observation, ObservationAdmin)
