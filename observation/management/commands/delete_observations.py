b859133e6cebf77bea0e7f809ea9b34518c61cb8
Bart Römgens <bart.romgens@gmail.com>
2017-09-01 16:50:30 +0200

    create command to delete all observations

---
+++ observation/management/commands/delete_observations.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+from observation.models import Observation
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Observation.objects.fall().delete()
