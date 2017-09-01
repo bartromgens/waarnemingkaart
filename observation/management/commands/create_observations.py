f014b4a0c24e72e4dc9449a0c887c141e13ee546
Bart Römgens <bart.romgens@gmail.com>
2017-09-01 14:27:01 +0200

    create observation models in management command

--- observation/management/commands/create_observations.py
+++ observation/management/commands/create_observations.py
@@ -4,15 +4,53 @@ from django.core.management.base import BaseCommand
 
 from scraper import scraper
 
+from observation.models import Observation
+from observation.models import Coordinates
+from observation.models import Group
+from observation.models import Family
+from observation.models import Species
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('date', nargs='+', type=str)
+        parser.add_argument('--max', type=int, help='The max number of observations to create.', default=None)
 
     def handle(self, *args, **options):
         date_str = options['date'][0]
+        max_n = options['max']
         date = dateparser.parse(date_str).date()
-        observations = scraper.get_observations_for_date(species_id=scraper.VOGELS_ID, date=date, max_n=1000)
+        observations = scraper.get_observations_for_date(species_id=scraper.VOGELS_ID, date=date, max_n=max_n)
         for observation in observations:
             observation.create()
             print(observation)
+            data = observation.data
+            species, created = Species.objects.get_or_create(
+                name_nl=data['name'],
+                name_latin=data['name_latin'],
+            )
+            family, created = Family.objects.get_or_create(
+                name_nl=data['family'],
+                name_latin=data['family_latin'],
+            )
+            group, created = Group.objects.get_or_create(
+                name_nl=data['group'],
+            )
+            if not data['coordinates']:
+                coordinates = None
+            else:
+                coordinates, created = Coordinates.objects.get_or_create(
+                    lat=data['coordinates']['lat'],
+                    lon=data['coordinates']['lon'],
+                )
+
+            Observation.objects.filter(url=data['url']).delete()
+            Observation.objects.create(
+                species=species,
+                family=family,
+                group=group,
+                number=data['number'],
+                datetime=data['datetime'],
+                coordinates=coordinates,
+                url=data['url'],
+            )
