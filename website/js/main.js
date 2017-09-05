4d2e7e3901c7ed09246685e7fe22e28ae91accf3
Bart Römgens <bart.romgens@gmail.com>
2017-09-05 15:30:22 +0200

    load the map data based on url arguments (group, family, species)

--- website/js/main.js
+++ website/js/main.js
@@ -11,32 +11,66 @@ $.ajaxSetup({
 });
 
 //var familySlug = 'all';
-var familySlug = 'aalscholvers';
+//var familySlug = 'aalscholvers';
 //var familySlug = 'boomklevers';
 //var familySlug = 'cettiidae';
 //var familySlug = 'haviken-en-arenden';
 //var familySlug = 'staartmezen';
 //var familySlug = 'duiven';
 //var familySlug = 'zwaluwen';
-//var familySlug = 'gierzwaluwen';
+var familySlug = 'gierzwaluwen';
 //var familySlug = 'flamingos';
 //var familySlug = 'eenden-ganzen-en-zwanen';
 //var familySlug = 'winterkoningen';
 //var familySlug = 'spreeuwen';
 
-var dataDir = "/static/data/";
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var group = getParameterByName('group');
+var family = getParameterByName('family');
+var species = getParameterByName('species');
+console.log('group', group);
+console.log('family', family);
+console.log('species', species);
+
+var observationFilepath = "/static/data/";
+var contoursFilepath = "/static/data/";
+
+if (group) {
+    observationFilepath += group + "/";
+    contoursFilepath += group + "/";
+}
+if (family !== "" && species !== "") {
+    observationFilepath += family + "/";
+    contoursFilepath += family + "/";
+    observationFilepath += species + ".json";
+    contoursFilepath += "contours_" + species + ".geojson";
+} else if (family !== "") {
+    observationFilepath += family + ".json";
+    contoursFilepath += "contours_" + family + ".geojson";
+}
+
+console.log(observationFilepath);
+console.log(contoursFilepath);
 
 var contourmap = observationmap.createObservationMap();
 
 var observationsLayer = null;
 
-$.getJSON(dataDir + "observations_" + familySlug + ".json", function(json) {
+$.getJSON(observationFilepath, function(json) {
     if (familySlug !== 'all') {
         observationsLayer = contourmap.createObservationsFeatureLayer(json.observations);
     }
 
-    var geojsonUrl = dataDir + "contours_" + familySlug + ".geojson";
-    contourmap.addContourTileLayer(geojsonUrl);
+    contourmap.addContourTileLayer(contoursFilepath);
     contourmap.map.on("moveend", updateVisibility);
 });
 
