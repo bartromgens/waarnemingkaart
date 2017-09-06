27078f79d85f3a0c23caaaf7b67c559b35f16854
Bart Römgens <bart.romgens@gmail.com>
2017-09-06 14:09:41 +0200

    use resolution as zoom factor

--- website/js/mapstyles.js
+++ website/js/mapstyles.js
@@ -1,20 +1,24 @@
 
 function observationFeatureStyle(feature, resolution) {
     var number = feature.get('number');
-    var zoom = 5.0; // TODO: function of resolution;
-    var zoomFactor = Math.pow(zoom, 2)/75.0;
-    var numberFactor = Math.pow(number, 1.0/2.0)/3.14;
+    var zoomFactor = Math.sqrt(5000/resolution);
+    var numberFactor = Math.pow(number/2.0, 1/3)/3.14;
     var strokeColor = 'blue';
     var circleColor = 'white';
 
     var radius = numberFactor*zoomFactor;
-    radius = Math.min(radius, 100.0*zoomFactor);
-    radius = Math.max(2.0*zoomFactor, radius);
-    width = 1.0*zoomFactor/1.5;
+//    radius = Math.min(radius, 100.0*zoomFactor);
+//    radius = Math.max(2.0*zoomFactor, radius);
 
     if (number > 200) {
         radius = 0;
     }
+
+    if (radius < 1) {
+        radius = 0;
+    }
+
+    width = 0.3*radius;
 
     var circleStyle = new ol.style.Circle(({
         fill: new ol.style.Fill({color: circleColor}),
