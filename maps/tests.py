19a85ba196aff708c81f1b48516a80e8c4466b54
Mathijs Romans <github@romansland.nl>
2017-09-29 15:10:31 +0200

    moved kdtree argument

---
+++ maps/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+
+class TestTest(TestCase):
+    """ Example test case. """
+
+    def test_asserts(self):
+        """ Example unit test. Tests unittest asserts. """
+        self.assertTrue(True)
+        self.assertEqual(True, True)
+        self.assertNotEqual(True, False)
+
+class TestTest(TestCase):
+    """ Example test case. """
+
+    def test_mathijs(self):
+        """ Example unit test. Tests unittest asserts. """
+        self.assertTrue(True)
+        self.assertEqual(True, True)
+        self.assertNotEqual(True, False)
+        print('hello')
